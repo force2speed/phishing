b3b36267494f610667f5e414f33829f09b9f7ee9
dhruv dhoundiyal <dhruvdhoundiyal@gmail.com>
2025-08-27 21:47:29 +0530

    first

--- crawl.test.js
+++ crawl.test.js
@@ -24,11 +24,14 @@ test("normalizeUrl strip capitals",()=>{
     const expected='blog.boot.dev/path';
     expect(actual).toEqual(expected);
 })
-test("getURLsFromHTML",()=>{
+test("getURLsFromHTML absolute",()=>{
     const inputHTMLBody=`
     <html>
         <body>
-            <a href="https://blog.boot.dev">
+            <a href="https://blog.boot.dev/path1/">
+                Boot.dev Blog
+            </a>
+            <a href="/path2/">
                 Boot.dev Blog
             </a>
         </body>
@@ -36,7 +39,23 @@ test("getURLsFromHTML",()=>{
     
     `;
     const inputBaseURL="https://blog.boot.dev";
-    const actual=getURLsFromHTML(input);
-    const expected=['blog.boot.dev/path'];
+    const actual=getURLsFromHTML(inputHTMLBody,inputBaseURL);
+    const expected=['https://blog.boot.dev/path1/','https://blog.boot.dev/path2/'];
+    expect(actual).toEqual(expected);
+})
+test("getURLsFromHTML invalid",()=>{
+    const inputHTMLBody=`
+    <html>
+        <body>
+            <a href="invalid">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    
+    `;
+    const inputBaseURL="https://blog.boot.dev";
+    const actual=getURLsFromHTML(inputHTMLBody,inputBaseURL);
+    const expected=[];
     expect(actual).toEqual(expected);
 })
