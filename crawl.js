3fbeedad5587e88c5c803a1206bafe35b2856216
dhruv dhoundiyal <dhruvdhoundiyal@gmail.com>
2025-08-28 21:05:46 +0530

    crawlpage

--- crawl.js
+++ crawl.js
@@ -1,4 +1,24 @@
 const {JSDOM}=require('jsdom')
+async function crawlPage(currentURL)
+{
+    console.log(`Actively Crawling ${currentURL}`);
+    try {
+        
+        const resp=await fetch(currentURL);
+        if(resp.status>399){
+            console.log(`error in fetch with status code: ${resp.status} on page  ${currentURL}`); 
+            return;
+        }
+        console.log(await resp.text());//already have geturls from html which will just take this text
+        const contentType=resp.headers.get("content-type");
+        if(!contentType.includes("text/html")){
+            console.log.log(`non html response, content type: ${contentType} on page  ${currentURL}`);  
+        }
+    } catch (error) {
+            console.log(`error in fetch: ${error.message} on page  ${currentURL}`);
+    }
+
+}
 function getURLsFromHTML(htmlBody,baseURL){
     const urls=[];
     const dom=new JSDOM(htmlBody);
@@ -43,5 +63,6 @@ return hostPath;
 
 module.exports={
     normalizeUrl,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
